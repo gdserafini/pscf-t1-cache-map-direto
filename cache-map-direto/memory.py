38c4e77a8fbedfffe36d203cbce22543d496f6ef
gdserafini <gdinizserafini@gmail.com>
2024-09-17 20:52:14 -0300

    update memory interface

--- cache-map-direto/memory.py
+++ cache-map-direto/memory.py
@@ -1,5 +1,7 @@
+from typing import Optional
+
 class MemoryInterface:
     def capacity(self) -> int: pass
-    def valid_address(self, address: int) -> bool: pass
-    def read(self, address: int) -> int: pass
+    def _valid_address(self, address: int) -> bool: pass
+    def read(self, address: int) -> Optional[int]: pass
     def write(self, address: int, value: int) -> None: pass
