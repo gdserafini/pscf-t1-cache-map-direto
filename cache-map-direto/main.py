de6c709723c0f1a9095f836ef6873edc04b9e213
Vinicius Yudi <vinizzly2005@gmail.com>
2024-09-19 15:51:28 -0300

    Comentarios "chat" retirados

--- cache-map-direto/main.py
+++ cache-map-direto/main.py
@@ -2,17 +2,17 @@ from cache import Cache
 from memory import RAM, EnderecoInvalido
 
 try:
-    ram = RAM(12)  # 4K de RAM (2**12)
-    cache = Cache(7, 4, ram)  # total cache = 128 (2**7), cacheline = 16 (2**4)
+    ram = RAM(12)  
+    cache = Cache(7, 4, ram)  
 
-    # Escrevendo valores na RAM
+    
     inicio = 0
     ram.write(inicio, 110)
     ram.write(inicio + 1, 130)
 
-    # Acessando os dados pela CPU (simulada)
-    print("Valor na cache após execução:", cache.access(inicio))  # Deve gerar cache miss, carregar da RAM e retornar 110
-    print("Valor na cache após execução:", cache.access(inicio + 1))  # Deve gerar cache miss, carregar da RAM e retornar 130
+    
+    print("Valor na cache após execução:", cache.access(inicio))  
+    print("Valor na cache após execução:", cache.access(inicio + 1))  
 
 except EnderecoInvalido as e:
-    print("Endereço inválido:", e.ender)  # Tratamento de exceção para endereços inválidos
+    print("Endereço inválido:", e.ender) 
